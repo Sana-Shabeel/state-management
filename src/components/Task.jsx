580259f60d5318d4e7d310ee6b2d90351169c0c5
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-08-28 15:47:04 +0300

    Add use zustand store

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import { useMemo } from "react";
+import { useStore } from "../store";
+import classNames from "classnames";
+import "./Task.css";
+
+const STATUS = "PLANNED";
 
 const Task = ({ title }) => {
-  return <div className="task">{title}</div>;
+  const tasks = useStore((store) => store.task);
+
+  // avoid re-rendering when tasks change
+  const findTask = useMemo(
+    () => tasks.find((task) => task.title === title),
+    [tasks, title]
+  );
+
+  console.log(findTask);
+
+  return (
+    <div className="task">
+      <div className="">{findTask.title}</div>
+      <div className="bottomWrapper">
+        <div></div>
+        <div className={classNames("status", findTask.state)}>
+          {findTask.state}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Task;

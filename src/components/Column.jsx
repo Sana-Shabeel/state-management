2d3e7b70418213fa9e287ffae807f906607e7483
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-08-28 15:46:51 +0300

    Add use zustand store

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import { useStore } from "../store";
+import Task from "./Task";
 import "./Column.css";
+import { useMemo } from "react";
 
 const Column = ({ state }) => {
-  return <div className="column">{state}</div>;
+  const tasks = useStore((store) => store.task);
+
+  // avoid re-rendering when tasks change
+  const filteredTasks = useMemo(
+    () => tasks.filter((task) => task.state === state),
+    [tasks, state]
+  );
+
+  console.log(filteredTasks);
+
+  return (
+    <div className="column">
+      <p>{state}</p>
+      {filteredTasks.map((task) => (
+        <Task key={task.title} title={task.title} />
+      ))}
+    </div>
+  );
 };
 
 export default Column;

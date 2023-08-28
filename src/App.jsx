b1833e0b63b300a26801fde7f8f016fed34f9c07
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-08-28 15:47:27 +0300

    Add content wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from "react";
 import Column from "./components/Column";
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <div className="App">
       <Column state="PLANNED" />
       <Column state="IN_PROGRESS" />
       <Column state="DONE" />
-    </>
+    </div>
   );
 }
 
